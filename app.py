568f49fa94b8e94084556280bd7162e453e0e000
Nicolas <ncmartinez018@gmail.com>
2025-05-06 14:56:22 -0500

    Menu principal

--- app.py
+++ app.py
@@ -1,23 +1,11 @@
-from flask import Flask, render_template_string
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route('/')
-def home():
-    return render_template_string("""
-    <!DOCTYPE html>
-    <html lang="en">
-    <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Página Básica</title>
-    </head>
-    <body>
-        <h1>Bienvenido a mi aplicación Flask</h1>
-        <p>Esta es una página básica.</p>
-    </body>
-    </html>
-    """)
-
 if __name__ == '__main__':
     app.run(debug=True)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
